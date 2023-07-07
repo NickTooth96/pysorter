1f9cb96b64a18737bc06ed588e9293bfa31d66a0
NickTooth96 <nicktoothaker@gmail.com>
2023-07-06 20:09:37 -0500

    WORKED on metadata retreival added unsort()

--- pysorter.py
+++ pysorter.py
@@ -1,18 +1,25 @@
 import datetime
 import os
+import shutil
 import sys
 import getopt
 import time
-import toml
+# import toml
+from PIL import Image
 
 
 __VERSION__ = "1.3.0"
 __AUTHOR__ = "Nicholas Toothaker"
 
+MONTHS = {  "01":"January","02":"February","03":"March",
+            "04":"April","05":"May","06":"June",
+            "07":"July","08":"August","09":"September",
+            "10":"October","11":"November","12":"December"}
+
 no_arg = "ERROR: No arguments given"
 versionMSG = "Version " + __VERSION__ + " by " + __AUTHOR__
 
-options = "a:hs:l:v"
+options = "u:hs:l:v"
 long_options = ["Help", "Sort", "List"]
 
 ### Class 
@@ -25,38 +32,88 @@ class Pysorter:
     dir_contents = []
 
     def __init__(self,**kwargs):
-        self.source_dir = kwargs['source_dir']
-        self.dest_dir = kwargs['dest_dir']
+        try:self.source_dir = kwargs['source_dir']
+        except: pass        
+        try:self.dest_dir = kwargs['dest_dir']
+        except: pass
         self.dest_structure = {}
-        self.dir_contents = self.get_list()
+        try:self.dir_contents = self.get_list()
+        except: pass
     
     def get_list(self):
-        res = []
-        for root, dirs, files in os.walk(self.source_dir, topdown=False):
-            for name in files:
-                res.append(name)
-        return res
+        buffer = []
+        output = []
+        buffer = os.listdir(self.source_dir)
+        for x in buffer:
+            if os.path.isfile(os.path.join(self.source_dir,x)):
+                output.append(x)
+        return output
     
-    def target_architecture(self):
+    def sort(self):
         years = []
-        months = ["January", "February", "March", 
-                  "April", "May", "June", 
-                  "July", "August", "September", 
-                  "October", "November", "December"]
-        res = {}
 
         # print("Contents:",self.dir_contents)
 
-        for x in self.dir_contents:            
-            t = os.stat(os.path.join(self.source_dir,x)).st_mtime
-            month = datetime.datetime.fromtimestamp(t).strftime("%m")
-            year = datetime.datetime.fromtimestamp(t).strftime("%y")
-            # print("\t",month,"/",year,"\n")
-            if year not in years:
-                years.append(year)
-                name = "20" + year
-                res[name] = months
-        print(res)
+        for x in self.dir_contents: 
+
+            try:
+                im = Image.open(os.path.join(self.source_dir,x))
+                ex = im.getexif()
+                # print(ex)
+                t = ex[306]
+                try:
+                    print("try:",t)
+                    t = t.timestamp()
+                    print("\t",t)
+                except:
+                    t = t
+                                
+            except:
+                t = os.stat(os.path.join(self.source_dir,x)).st_mtime
+                print("except:",t)
+
+            # month = datetime.datetime.fromtimestamp(t).strftime("%m")
+            # year = datetime.datetime.fromtimestamp(t).strftime("%y")
+            # name = "20" + year    
+            # dest = os.path.join(self.dest_dir,name)
+            # if os.path.exists(dest):
+            #     final_dest = os.path.join(dest,MONTHS[month])
+            #     if os.path.exists(final_dest):
+            #         shutil.move(os.path.join(self.source_dir,x),final_dest)
+            #     else:
+            #         os.makedirs(os.path.join(dest,MONTHS[month]))
+            #         shutil.move(os.path.join(self.source_dir,x),final_dest)
+            # else:
+            #     os.makedirs(os.path.join(dest,MONTHS[month]))
+            #     shutil.move(os.path.join(self.source_dir,x),os.path.join(dest,MONTHS[month]))
+
+    def unsort(self):
+        file_count = 0
+        buffer = os.listdir(self.source_dir)
+       
+        for x in buffer:   
+            path_0 = os.path.join(self.source_dir,x)          
+            if os.path.isdir(path_0) and "20" in x:
+                buffer = os.listdir(path_0)   
+
+                for x in buffer: 
+                    path_1 = os.path.join(path_0,x) 
+                    if os.path.isdir(path_1) and x in MONTHS.values():
+                        buffer = os.listdir(path_1) 
+
+                        for x in buffer:
+                            path_2 = os.path.join(path_1,x)
+                            if os.path.isfile(path_2):
+                                file_count += 1
+                                shutil.move(path_2,self.source_dir)
+                                
+                            else:
+                                print("NONFILE FOUND")
+                # print(path_0)
+                shutil.rmtree(path_0)
+
+            
+                
 
 
 
@@ -65,31 +122,32 @@ class Pysorter:
 
 
 
-
+### MAIN
 
 arglist = sys.argv[1:]
 
 sys.argv.pop(0)
-print(sys.argv)
+# print(sys.argv)
 
 if len(arglist) < 1:
-    print(no_arg)
+    # print(no_arg)
     raise SystemExit
 
 try: 
     arguments, values = getopt.getopt(arglist, options, long_options)
     for currentArgument, currentValue in arguments:
         if currentArgument in ("-s", "--sort"):
-            print("Sort")
+            sorter = Pysorter(source_dir=arglist[1], dest_dir=arglist[2])
+            sorter.sort()
+            print("Has run")
         elif currentArgument in ("-l","--list"):
             print("List")
             sorter = Pysorter(source_dir=arglist[1], dest_dir=None)
             print(sorter.source_dir)
             print(sorter.dir_contents)
-        elif currentArgument in ("-a"):
-            print("Creating Architecture...")
-            sorter = Pysorter(source_dir=arglist[1], dest_dir=None)
-            sorter.target_architecture()
+        elif currentArgument in ("-u","unsort"):
+            sorter = Pysorter(source_dir=arglist[1])
+            sorter.unsort()
             print("Has run")
         elif currentArgument in ("-h", "--Help"):
             path = os.path.join(os.path.dirname(__file__), ".help.txt")
